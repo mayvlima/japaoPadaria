2ebb9108d51611b569d1b5f0ff9e62501604ce70
Virginia <virginia_hol@hotmail.com>
2021-07-28 13:12:56 -0300

    Correcao de  erro item_venda

--- src/main/java/com/techbank/japaoPadaria/repository/ItemVendaRepository.java
+++ src/main/java/com/techbank/japaoPadaria/repository/ItemVendaRepository.java
@@ -3,9 +3,10 @@ package com.techbank.japaoPadaria.repository;
 import com.techbank.japaoPadaria.model.ItemVenda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
-    @Repository
+
 
     public interface ItemVendaRepository extends JpaRepository<ItemVenda, Long> {
-}
+    }
 
