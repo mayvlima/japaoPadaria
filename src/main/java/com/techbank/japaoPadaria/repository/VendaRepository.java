f079302b07aa45995d6f3381a451d6e7fbdee294
Virginia <virginia_hol@hotmail.com>
2021-07-30 11:32:01 -0300

    atualizacao venda

--- src/main/java/com/techbank/japaoPadaria/repository/VendaRepository.java
+++ src/main/java/com/techbank/japaoPadaria/repository/VendaRepository.java
@@ -2,10 +2,20 @@ package com.techbank.japaoPadaria.repository;
 
 import com.techbank.japaoPadaria.model.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 
 public interface VendaRepository extends JpaRepository<Venda, Long> {
+
+    //@Query(value = "SELECT v FROM Venda v")
+    List<Venda> findAllByVenda(Venda venda);
+
+
+    @Query(value = "SELECT v.Movimentacao FROM Venda v")
+    List<Venda> findAllByDataDeMovimentacaoBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
 
 }
